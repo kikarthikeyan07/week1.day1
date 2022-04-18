c7a41814a4ba714a678eb7020b1157d86f3a9630
kikarthikeyan07 <103938826+kikarthikeyan07@users.noreply.github.com>
2022-04-19 00:17:17 +0530

    Add files via upload

---
+++ Assignments/Convertnegativetopositive.java
@@ -0,0 +1,11 @@
+package week1.day1.Assignments;
+
+public class Convertnegativetopositive {
+public static void main(String[] args) {
+int i = -40;
+if (i<0) {
+	int j =i*(-1);
+	System.out.println(i+ " has got changed to "+j);
+}
+}
+}
